a0b9dbb2c2d075116286777365e3d22a19b4abf8
DBoratyn <daggi.bor97@gmail.com>
2019-06-07 05:33:13 +0200

    Automation Testing

---
+++ StacjaBenzyowa_TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StacjaBenzyowa_TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StacjaBenzyowa_TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3c8e8d0-1a5a-4192-ba24-b8cf390fb5ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StacjaBenzyowa_TEST/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using StacjaBenzynowa.Models;
+using System;
+using System.Threading;
+using SQLite;
+using StacjaBenzynowa;
+
+namespace StacjaBenzyowa_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        protected static WindowsDriver<WindowsElement> desktopSession;
+        [TestMethod]
+        public void Open()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", @"C:\Users\Jehan\Documents\GitHub\GasStation-WPF\StacjaBenzynowa\bin\Debug\StacjaBenzynowa.exe");
+            desktopSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+            Assert.IsNotNull(desktopSession.FindElementByAccessibilityId("loginBtn"));
+        }
+
+        [TestMethod]
+        public void LogInAsAdmin()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", @"C:\Users\Jehan\Documents\GitHub\GasStation-WPF\StacjaBenzynowa\bin\Debug\StacjaBenzynowa.exe");
+            appCapabilities.SetCapability("Login", @"C:\Users\Jehan\Documents\GitHub\GasStation-WPF\StacjaBenzynowa\bin\Debug\StacjaBenzynowa.exe");
+            desktopSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+
+            desktopSession.FindElementByAccessibilityId("loginBtn").Click();
+            desktopSession.FindElementByAccessibilityId("loginbox");
+
+            //doesnt work 
+            desktopSession.FindElementByAccessibilityId("loginBox").Click();
+            desktopSession.FindElementByAccessibilityId("loginBox").SendKeys("jankowalski");
+
+            desktopSession.FindElementByAccessibilityId("passwordBox").Click();
+            desktopSession.FindElementByAccessibilityId("passwordBox").SendKeys("kowalski");
+
+            desktopSession.FindElementByAccessibilityId("ZalogujBtn").Click();
+            Assert.IsNotNull(desktopSession.FindElementByAccessibilityId("MonitoringBtn"));            
+        }
+    }
+}
